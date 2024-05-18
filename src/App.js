861ddc69f1e0893af3e1a49a15bb31cbb6a33a05
czudec <kaczudec@gmail.com>
2024-05-18 11:55:01 +0200

    Interakcje

--- src/App.js
+++ src/App.js
@@ -2,30 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 function App() {
-    const [email, setEmail] = useState('fracz@agh.edu.pl');
-
+    const [email, setEmail] = useState('');
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     function handleChange(event) {
         setEmail(event.target.value);
     }
 
+    function logout() {
+        setIsAuthenticated(false);
+        setEmail(" ");
+    }
 
-    let message;
-    if (email.length < 10) {
-        message = <div>Ale masz krótki adres!</div>;
-    } else if (email.length < 15) {
-        message = <div>Twój adres e-mail jest w sam raz.</div>;
+    let content;
+    if (isAuthenticated){
+    content = <div>
+        <h2> Witaj {email}!</h2>
+        <button onClick={logout}> Wyloguj </button>
+    </div>
     } else {
-        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
+        content = <div>
+            Zaloguj się e-mailem
+            <input type="text" value={email} onChange={handleChange} />
+            <button onClick={() => setIsAuthenticated(true)}>Wchodzę</button>
+        </div>
     }
 
     return (
         <div>
-            <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}</h2>
-            {message}
-            <input type="text" value={email} onChange={handleChange} />
-            <button onClick={() => alert(email)}>Wyświetl mój adres w alercie</button>
+            <h1>Witaj w systemie do zapisów na zajęcia</h1>
+            {content}
         </div>
     );
 
