281730d56ec9bf08dd2df805003ed2797f14ad5f
czudec <kaczudec@gmail.com>
2024-05-18 11:05:47 +0200

    Reaktywność

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
 function App() {
-    let email = 'kczudec@agh.edu.pl';
+    const [email, setEmail] = useState('fracz@agh.edu.pl');
+
 
     function handleChange(event) {
+        setEmail(event.target.value);
+    }
 
-        console.log(event.target.value);
-
+    let message;
+    if (email.length < 10) {
+        message = <div>Ale masz krótki adres!</div>;
+    } else if (email.length < 15) {
+        message = <div>Twój adres e-mail jest w sam raz.</div>;
+    } else {
+        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email.toUpperCase()}</h2>
-            <input type="text" onChange={handleChange}/>
+            <h2>Twój e-mail to {email}</h2>
+            {message}
+
+            <input type="text" value={email} onChange={handleChange}/>
         </div>
     );
+
 }
 
 export default App;
